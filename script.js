420484c2902ac4ec59370ea6e2dd7bd7720db358
Cтас <90937465+sfirsovdev@users.noreply.github.com>
2022-06-01 01:25:08 +0300

    Test

---
+++ script.js
@@ -0,0 +1,10 @@
+function palindrom(str) {
+    //нижний регистр
+    str = str.toLowerCase();
+    //строку в массив
+    let str2 = str.split('');
+
+    return str2;
+}
+
+console.log(palindrom('hello'));
